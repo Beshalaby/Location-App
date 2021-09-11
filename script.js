e402781a1bec651c94df063763d6b06ae04bf328
Beshalaby <68404054+Beshalaby@users.noreply.github.com>
2021-09-11 19:37:23 -0400

    Revert "added landmarks"

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 var map;
 var marker
 
@@ -22,10 +24,6 @@ function addMarker (coords, title, popup) {
     icon:"pictures/Smol Marker.png" 
 });
 
-function thingy(coords, title, popup) {
-  addMarker(coords, title, popup);
-  marker.setMap(map);
-}
 
 marker.addListener('click', function () {
   document.getElementById("hi").innerHTML=popup;
@@ -35,18 +33,14 @@ marker.addListener('click', function () {
 
 
 //to add marker use the addMarker function like this {lat:, lng:} , then title of marker, then you say what the marker is
-addMarker({lat:39.61629658815152, lng:-77.65704027976375}, "landmark", "description");
-addMarker({lat:39.47816050082987, lng:-77.71434117301506}, "Antietam National Battlefield", "placeholder");
-addMarker({lat:39.51532141437621, lng:-77.63052100986665}, "Washington Monument", "placeholder");
-addMarker({lat:39.35555044758504, lng:-77.72229239966968}, "Maryland Heights", "placeholder");
-addMarker({lat:, lng:}, "", "");
-addMarker({lat:, lng:}, "", "");
-addMarker({lat:, lng:}, "", "");
-addMarker({lat:, lng:}, "", "");
-addMarker({lat:, lng:}, "", "");
+addMarker({lat:39.61629658815152, lng:-77.65704027976375}, "my house!", "my house!");
+
+
 
 // To add the marker to the map, call setMap();
-// marker.setMap(map);
+marker.setMap(map);
+
+
 
 }
 
